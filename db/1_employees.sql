4169c2f451b58b0ce6e2189488e8d88586ddd0ab
edarreche <esteban.darreche@softtek.com>
2017-12-14 20:37:24 -0300

    basic docker compose + Db creation

---
+++ db/1_employees.sql
@@ -0,0 +1,34 @@
+--  Sample employee database
+--  See changelog table for details
+--  Copyright (C) 2007,2008, MySQL AB
+--
+--  Original data created by Fusheng Wang and Carlo Zaniolo
+--  http://www.cs.aau.dk/TimeCenter/software.htm
+--  http://www.cs.aau.dk/TimeCenter/Data/employeeTemporalDataSet.zip
+
+
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE IF NOT EXISTS employees;
+USE employees;
+
+SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
+
+DROP TABLE IF EXISTS employees;
+
+/*!50503 set default_storage_engine = InnoDB */;
+/*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
+
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+flush /*!50503 binary */ logs;
+
+SELECT 'LOADING employees' as 'INFO';
+/*source load_employees.dump ;*/
---
+++ readme.txt
@@ -0,0 +1,15 @@
+# Install Docker & git
+$apt-get install docker docker-compose git
+
+#checkout the project
+git clone xxxxx
+
+cd xxxxx
+
+#create the environment
+$docker-compose up
+
+#access the App
+
+#destroy the environment
+$docker-compose down
